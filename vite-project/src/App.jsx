98d26534ba13bbfa9fe79248c13ebb38889474f0
Hrishikesh kocharekar <127331666+hrishikesh-kocharekar-dev@users.noreply.github.com>
2023-07-29 02:54:12 +0530

    Initial commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import "./App.css";
+import Auth from "./components/Auth";
+
+function App() {
+  return (
+    <div className="App">
+      <Auth />
+    </div>
+  );
+}
+
+export default App;
---
+++ vite-project/src/config/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+import { getAuth, GoogleAuthProvider, GoogleAuthProvider } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBDg1TxDY7asCA1EuRnKqqEPWUKzKpWfOI",
+  authDomain: "fir-doc-7436f.firebaseapp.com",
+  projectId: "fir-doc-7436f",
+  storageBucket: "fir-doc-7436f.appspot.com",
+  messagingSenderId: "325841503197",
+  appId: "1:325841503197:web:a7b7f508d8f156e3572c34",
+  measurementId: "G-NTM39Y7L8N",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const GoogleAuthProvider = new GoogleAuthProvider();
